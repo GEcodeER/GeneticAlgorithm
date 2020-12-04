de2e55992b8b36a6984427982651a7f42963f7fe
langeli <leonardo.angeli@eng.it>
2020-12-04 11:52:17 +0100

    Removed unnecessary constants

--- src/com/gecoder/geneticalgo/util/Constants.java
+++ src/com/gecoder/geneticalgo/util/Constants.java
@@ -2,18 +2,6 @@ package com.gecoder.geneticalgo.util;
 
 public class Constants {
 
-    public static final int VERTICAL_HORIZONTAL_COST = 10;
-    public static final int NUM_ROWS = 6;
-    public static final int NUM_COLUMNS = 10;
-
-    public static final int NUMBER_OF_ITERATIONS = 100000;
-    public static final int SIZE_OF_TABU_QUEUE = 400;
-    public static final int NUM_OF_VALUES = 200;
-
-    public static final int MAX_TEMPERATURE = 100;
-    public static final int MIN_TEMPERATURE = 1;
-    public static final double COOLING_RATE = 0.02;
-
     public static final int[] SOLUTION_SEQUENCE = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
             , 12, 13, 14, 15};
     public static final double CROSSOVER_RATE = 0.05;
